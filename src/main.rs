a7253c032c173d225bfb16427877d77a7a163eec
SUN-LG <slgspace@outlook.com>
2026-07-09 20:48:38 +0800

    feat: Add env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use env_logger::Env;
 use news_letter::{config::get_config, startup::run};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
   let config = get_config().expect("Failed to read config");
   let connection_pool = PgPool::connect(&config.database.connection_string())
     .await
